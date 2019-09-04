4b3b79ec4b0ba780cbf0f62b08ef5d8b89de1993
123456-cq <2992018384@qq.com>
2019-09-04 17:00:13 +0800

    wison

---
+++ xkq34_src/Hidistro.SaleSystem.Vshop/IntegralOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Hidistro.SaleSystem.Vshop
+{
+    public class IntegralOperation
+    {
+        SqlHelper help = new SqlHelper();
+
+        #region 查询商品积分信息
+        public DataTable GetProductIntegralInfo(int productId)
+        {
+            DataTable dt = null;
+            try
+            {
+                string sql = "select [integral],[IntegralToNow],[IntegralCeiling] from [dbo].[Hishop_Products] where [ProductId]=@ProductId";
+                SqlParameter[] param = new SqlParameter[]{
+                             new SqlParameter("@ProductId",productId)
+                        };
+                dt = help.GetTable(sql,CommandType.Text,param);
+            }
+            catch (Exception)
+            {
+                return dt;
+                throw;
+            }
+            return dt;
+        }
+        #endregion
+    }
+}
